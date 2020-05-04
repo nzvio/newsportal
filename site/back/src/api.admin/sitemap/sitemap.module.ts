8e9832193a37f9a5efe85d0fb711afa4f441600b
alex zdorov <7573497@gmail.com>
2020-05-04 17:03:43 +0300

    sitemap

--- site/back/src/api.admin/sitemap/sitemap.module.ts
+++ site/back/src/api.admin/sitemap/sitemap.module.ts
@@ -8,13 +8,17 @@ import { SitemapService } from "./sitemap.service";
 import { SitemapController } from "./sitemap.controller";
 import { SettingSchema } from "../../model/orm/schemas/setting.schema";
 import { LangSchema } from "../../model/orm/schemas/lang.schema";
+import { PageSchema } from "../../model/orm/schemas/page.schema";
+import { CategorySchema } from "../../model/orm/schemas/category.schema";
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             {name: 'Article', schema: ArticleSchema},
             {name: 'Setting', schema: SettingSchema},
-            {name: 'Lang', schema: LangSchema}
+            {name: 'Lang', schema: LangSchema},
+            {name: 'Page', schema: PageSchema},
+            {name: 'Category', schema: CategorySchema},
         ]),        
         JwtModule.register(jwtConstants),
     ],
--- site/back/src/api.admin/sitemap/sitemap.service.ts
+++ site/back/src/api.admin/sitemap/sitemap.service.ts
@@ -8,13 +8,17 @@ import { APIService } from "../../common.services/_api.service";
 import { IAnswer } from "../../model/answer.interface";
 import { ISetting } from "../../model/orm/interfaces/setting.interface";
 import { ILang } from "../../model/orm/interfaces/lang.interface";
+import { IPage } from "../../model/orm/interfaces/page.interface";
+import { ICategory } from "src/model/orm/interfaces/category.interface";
 
 @Injectable()
 export class SitemapService extends APIService {
-    constructor (
-        @InjectModel("Article") private readonly modelArticle: Model<IArticle>,
+    constructor (        
         @InjectModel("Setting") private readonly modelSetting: Model<ISetting>,
         @InjectModel("Lang") private readonly modelLang: Model<ILang>,
+        @InjectModel("Page") private readonly modelPage: Model<IPage>,
+        @InjectModel("Category") private readonly modelCategory: Model<ICategory>,
+        @InjectModel("Article") private readonly modelArticle: Model<IArticle>,
     ) {
         super();
     }
@@ -63,19 +67,36 @@ export class SitemapService extends APIService {
 
             let root: string = setting.v;
             let data: string = 
-                `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` + 
+                `<?xml version="1.0" encoding="UTF-8"?>\n`+
+                `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` + 
                 `<url><loc>${root}</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.5</priority></url>\n`;
-            let langs: ILang[] = await this.modelLang.find({active: true});
+            let langs: ILang[] = await this.modelLang.find({active: true}, {slug: 1, sluggable: 1});
+            let pages: IPage[] = await this.modelPage.find({active: true}, {slug: 1});
+            let categories: ICategory[] = await this.modelCategory.find({active: true}, {slug: 1});
 
             for (let lang of langs) {
                 // home page
                 if (!lang.sluggable) {
-                    data += `<url><loc>${root}/${lang.slug}</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.5</priority></url>\n`;
+                    data += `<url><loc>${root}/${lang.slug}</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.5</priority></url>\n\n`;
+                }                
+                
+                // pages
+                for (let page of pages) {
+                    data += `<url><loc>${root}/${lang.slug}/${page.slug}</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.5</priority></url>\n`;
                 }
 
-                data += "\n";
+                // categories
+                for (let category of categories) {
+                    data += `<url><loc>${root}/${lang.slug}/catalogue/category/${category.slug}</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.5</priority></url>\n`;
+                    let articles: IArticle[] = await this.modelArticle.find({active: 1, category: category._id, lang: lang._id}, {slug: 1});
+
+                    for (let article of articles) {
+                        data += `<url><loc>${root}/${lang.slug}/catalogue/category/${category.slug}/${article.slug}</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.5</priority></url>\n`;    
+                    }
+                }
             }
 
+            data += "</urlset>";
             return {statusCode: 200, data};
         } catch (err) {
             let errTxt: string = `Error in SitemapService.build: ${String(err)}`;
